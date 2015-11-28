480a13a6e4dc48ba6c869d171c326ba166c1941b
Gradon <gradon.faulkner931@myci.csuci.edu>
2015-11-28 10:10:19 -0800

    Adding test files

---
+++ ruby/sorting_main.rb
@@ -0,0 +1,15 @@
+# somefile.rb
+
+class MyClass
+  def say_hello
+    puts "Hello World"
+  end
+end
+
+def another_hello
+  puts "Hello World (from a method)"
+end
+
+c = MyClass.new
+c.say_hello
+another_hello
